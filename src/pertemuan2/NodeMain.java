901dc4fe06e319a1be1d804cc5a661530c633e2f
mildakhaerunnisa <mildakhaerunnisa25@gmail.com>
2025-02-12 11:34:48 +0700

    mengumpulkan pertemuan 2

--- src/pertemuan2/NodeMain.java
+++ src/pertemuan2/NodeMain.java
@@ -3,45 +3,47 @@ package pertemuan2;
 public class NodeMain {
 	public static void main(String[] args) {
 		
-		//membuat 5 buah node 
+		//Membuat 2 Buah Node n1 & n2
 		Node n1 = new Node(5);
 		Node n2 = new Node(7);
+		
+		//Menambahkan 2 Buah Node n3 & n4		
 		Node n3 = new Node(9);
 		Node n4 = new Node(8);
+		
+		//Menambahkan 2 Buah Node n5 & n6
 		Node n5 = new Node(2);
 		Node n6 = new Node(3);
 		
-		//membuat relasi node  n1 dengan node n2
-		n1.setNext(n2 );
+		//Membuat relasi untuk node n1 dan n2
+		n1.setNext(n2);
 		
-		//membuat relasi node n2 dengan node n3
+		//Membuat relasi untuk node n2 dan n3
 		n2.setNext(n3);
 		
-		//membuat relasi node n3 dengan node n4
+		//Membuat relasi untuk node n3 dan n4
 		n3.setNext(n4);
 		
-		//membuat relasi node n4 dengan node n5
-		n4.setNext(n5);
-		
-		//membuat relasi node n5 dengan node n6
+		//Membuat relasi untuk node n5 dan n6
 		n5.setNext(n6);
 		
-		//membuat relasi node n6 dengan node n1
-		n1.setNext(n1);
+		//Membuat relasi untuk node n6 dan n1
+		n6.setNext(n1);
 		
-		//menampilkan Node dengan pointer p
+		//Menampilkan Node dengan pointer p dan p1
 		Node p = n1;
-		while (p != null)
+		Node p1 = n5;
+		while(p != null)
 		{
-			System.out.printf("%d ",p.getNilai());
+			System.out.printf("%d ", p.getNilai());
 			p = p.getNext();
 		}
-		//menampilkan node yang akan berhenti di node ke n4
-		System.out.println();
-		while (p != n4) 
+		System.out.println("");
+		while(p1 != n4)
 		{
-			System.out.printf("%d ",p.getNilai());
-			p = p.getNext();
+			System.out.printf("%d ", p1.getNilai());
+			p1 = p1.getNext();
 		}
 	}
+	
 }
