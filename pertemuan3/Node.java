756c0c314913715fa1ef61bed028faf760742e94
mildakhaerunnisa <146157113+mildakhaerunnisa@users.noreply.github.com>
2025-02-19 21:09:43 +0700

    pertemuan 3

---
+++ pertemuan3/Node.java
@@ -0,0 +1,28 @@
+package pertemuan3;
+
+public class Node {
+
+	private int data;
+	private Node Next;
+	
+	//inisialisasi Node
+	public Node(int data) {
+		this.data = data;
+	}
+	
+	public void setData (int data) {
+		this.data=data;
+	}
+	
+	public int getData() {
+		return data;
+	}
+	
+	public void setNext (Node Next) {
+		this.Next = Next;
+	}
+	
+	public Node getNext() {
+		return Next;
+	}
+}
